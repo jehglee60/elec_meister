eb002d351de02eb2334a4f70056a054be4b80bb3
JeHyung Lee <jehglee60@gmail.com>
2024-12-17 17:31:49 +0900

    Resolve merge conflict in content_repository.dart

--- lib/data/content_repository.dart
+++ lib/data/content_repository.dart
@@ -1,9 +1,5 @@
 import 'dart:convert';
-<<<<<<< HEAD
 import 'package:flutter/services.dart';
-=======
-import 'package:flutter/services.dart' show rootBundle;
->>>>>>> ea3f20d43a1173665ae99c2c07cc4b9090628888
 import 'package:test_rendering/models/content.dart';
 
 class ContentRepository {
@@ -11,11 +7,6 @@ class ContentRepository {
     final String response =
         await rootBundle.loadString('assets/data/content.json');
     final List<dynamic> data = json.decode(response);
-<<<<<<< HEAD
     return data.map((json) => Content.fromJson(json)).toList();
-=======
-
-    return data.map((item) => Content.fromJson(item)).toList();
->>>>>>> ea3f20d43a1173665ae99c2c07cc4b9090628888
   }
 }
